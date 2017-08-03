cf12e33be8dc687cf18ed3ba6f21ce503053c249
goedan <dgoedeker@gmail.com>
2017-08-03 18:12:42 -0500

    plot 2 code

--- plot2.R
+++ plot2.R
@@ -1,9 +1,9 @@
 source("./ExploratoryData1/loadData.R")
 
 # Save to png
-
 png(file = "./ExploratoryData1/plot2.png", height = 480, width = 480, units = "px")
 
-plot(hpc$Time, hpc$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts")
+plot(hpc$Time, hpc$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts"))
 
+# close the png graphics device
 dev.off()
