a8848e857d2d3984f0a2a536e7e70c2ddbb2cf4f
goedan <dgoedeker@gmail.com>
2017-08-03 17:15:48 -0500

    plot 1 code

--- plot1.R
+++ plot1.R
@@ -1,11 +1,9 @@
 source("./ExploratoryData1/loadData.R")
 
 # Save to png
-
 png(file = "./ExploratoryData1/plot1.png", height = 480, width = 480, units = "px")
 
 hist(hpc$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 
 # close the png graphics device
-
 dev.off()
